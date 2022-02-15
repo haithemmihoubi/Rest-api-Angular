3e29bd16e3539291bd85bc97552ceaf702674166
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-02-15 12:21:31 +0100

    fetching data from https://jsonplaceholder.typicode.com/posts and subscribe to the api

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {ShareServiceService} from "../services/share-service.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  dataArray: any
+  dataObservable: Subscription
 
-  constructor(private  http:HttpClient) {
-    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe((data: any)=>console.log(data))
+  constructor(private share: ShareServiceService, private http: HttpClient) {
+    //Subscribe to the API to fetch data from the current url in getAllPosts
+    this.dataObservable = this.share.getAllPosts().subscribe((data: any) => this.dataArray = data)
+
 
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.dataObservable.unsubscribe()
   }
 
 }
---
+++ src/app/services/share-service.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShareServiceService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  /*
+  * get All POSTS from jsonPlaceHolder
+  * */
+  getAllPosts() {
+    return this.http.get('https://jsonplaceholder.typicode.com/posts')
+  }
+
+}
